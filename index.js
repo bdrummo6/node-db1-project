f743819852636f0384c37622ec46132cf249df99
bdrummo6 <bradley.drummonds@gmail.com>
2020-06-16 17:47:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require('./server.js');
 
 const PORT = process.env.PORT || 8000;
 
+server.get('/', (req, res) => {
+  res.send(`The server is running on port ${PORT}`)
+})
+
 server.listen(PORT, () => {
   console.log(`API running on port ${PORT}`);
 });
