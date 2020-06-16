7ecbf1b09328b335b23f48d840a5fe0ae0105831
bdrummo6 <bradley.drummonds@gmail.com>
2020-06-16 16:12:27 -0400

    Endpoint for creating an account was added

--- api/account-router.js
+++ api/account-router.js
@@ -40,6 +40,29 @@ router.get('/:id', (req, res) => {
 		})
 })
 
+// Creates a new Account
+router.post('/', (req, res) => {
 
+	// If the following fields are not completed then no account will be created
+	if (!req.body.name || !req.body.budget) {
+		return res.status(400).json({
+			errorMessage: 'Please provide name and budget for the account.'
+		})
+	}
+
+	db('accounts').insert(req.body)
+		.then((account) => {
+			console.log(account)
+			res.status(200).json({
+				message: 'The account was successfully added.'
+			})
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({
+				errorMessage: 'There was an error in posting the account.'
+			})
+		})
+})
 
 module.exports = router;
